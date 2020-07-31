85bbf35af6c6a304c22e3c9b9bb20319ddea6ade
shingli <745573545@qq.com>
2020-07-31 23:53:56 +0800

    http

--- admin/src/config/http.js
+++ admin/src/config/http.js
@@ -1,14 +1,36 @@
 import axios from 'axios'
-// import { message } from 'element-ui'
+import { getCookie } from 'config/http'
+import { message } from 'element-ui'
 
 const instance = axios.create({
     baseURL: process.env.VUE_APP_BASEURL,
     timeout: 5000
 })
 
-console.log(process.env.VUE_APP_BASEURL)
-instance.interceptors.request.use(config => {
-    return config
-})
+instance.interceptors.request.use(
+    config => {
+        config.headers['Authorization'] = getCookie()
+        return config
+    },
+    err => {
+        return Promise.reject(err)
+    }
+)
+
+instance.interceptors.response.use(
+    response => {
+        let { data: { responseCode, responseMsg } } = response
+        if (responseCode != '0000') {
+            message({
+                message: responseMsg || '返回值错误',
+                type: 'error',
+                duration: 1500
+            })
+        } else return data
+    },
+    err => {
+        return Promise.reject(err)
+    }
+)
 
 export default instance
