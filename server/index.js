32b549284d204803dd6ae06dc2acf354cce38df4
shingli <745573545@qq.com>
2020-08-03 23:31:37 +0800

    serve

--- server/index.js
+++ server/index.js
@@ -9,17 +9,28 @@ const host = process.env.HOST || 'localhost'
 
 app.use(cors())
 
+const presetdir = ['models', 'plugins']
 const routesdir = __dirname + '/routes'
 
+const global = fs.readdirSync(__dirname, 'utf-8').filter((dir) => presetdir.includes(dir)).reduce((total, cur) => {
+	fs.readdirSync(`${__dirname}/${cur}`, 'utf-8').map(item => {
+		let name = item.replace(/.\js/, '')
+		total[cur][name] = require(`${__dirname}/${cur}/${name}`)
+	})
+	return total
+}, { plugins: {}, models: {} })
+
+console.log(global)
 fs.readdirSync(routesdir, 'utf-8').forEach(dir => {
 	dir = routesdir + '/' + dir
 	if (fs.statSync(dir).isDirectory()) {
 		fs.readdirSync(dir).forEach(route => {
 			route = route.replace(/\.js$/, '')
-			require(`${dir}/${route}`)(app)
+			require(`${dir}/${route}`)(app. global['models'])
 		})
 	}
 })
+
 
 http.createServer(app).listen(port, () => {
 	console.log(`serve running ${host}:${port}`)
