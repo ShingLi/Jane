8f75ed9995db5279cb5af526dcaf4f725f179d32
shingli <745573545@qq.com>
2020-08-11 22:37:58 +0800

    serve

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1/runoob', { 
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
 
 module.exports = () => {
-	const db = mongoose.connection
-
-	db.on('error', console.error.bind(console, '连接数据库失败'))
-
-	db.on('open', () => {
-		console.log('~连接数据库成功～')
-	})
+	mongoose.connect('mongodb://127.0.0.1/runoob', { 
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}).then(
+		() => {
+			console.log('~连接数据库成功～')
+		},
+		err => {
+			console.error.bind(console, err || '连接数据库失败')
+		}
+	)
 }
