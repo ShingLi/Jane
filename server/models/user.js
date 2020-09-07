dc0e3eb96c45fc2de6b530503eda159a11a6ed0a
shingli <745573545@qq.com>
2020-09-07 23:23:32 +0800

    fix :bug:

--- server/models/user.js
+++ server/models/user.js
@@ -3,18 +3,22 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
 	username: {
 		type: Number,
-		min: [2, '最少一位'],
-		max: [6, '最多六位'],
-		required: true
+		required: [ true, '密码不能为为空'],
+		validate: {
+			validator: username => {
+				return (username + '').length > 2
+			},
+			message: '密码长度太多，最少2位'
+		}
 	},
 	password: {
 		type: String,
 		required: true,
 		validate: {
-			validator: function (val) {
-				return val
+			validator: pwd => {
+				return (pwd + '').length > 6
 			},
-			message: '密码太短'
+			message: '密码为6位数字'
 		}
 	}
 })
