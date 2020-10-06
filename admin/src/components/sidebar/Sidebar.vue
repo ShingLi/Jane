115f229cff93cb0de4d8fb28642e268f73eccd8c
shingli <745573545@qq.com>
2020-10-06 15:10:07 +0800

    feat: 功能

--- admin/src/components/sidebar/Sidebar.vue
+++ admin/src/components/sidebar/Sidebar.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="sidebar">
         <div id="avatar"
-            class="avatar"
         >
-            <Avatar :src="avatar" class="custom--avatar"/>
+            <Avatar :src="userInfo.avatar" class="custom--avatar avatar"/>
+            <p class="username">{{ userInfo.name }}</p>
         </div>
         <ol class="menu">
-            <li v-for="(n, i) of menu" :key="i">{{ n }}</li>
+            <li v-for="(v, i) of menuList"
+                :key="i"
+            >{{ v.title }}</li>
         </ol>
     </div>
 </template>
 <script>
-import Avatar from './modules/avatar.jsx'
+import Avatar from './modules/avatar'
 export default {
     name: 'Sidebar',
     components: {
@@ -19,8 +21,21 @@ export default {
     },
     data () {
         return {
-            avatar: 'https://thirdwx.qlogo.cn/mmopen/sHGrggDoMxFfm6HEt7P6DRQ7AnxBJ7SgADdfhLib2V4z7iaGvNWytAZSMC9NCNZ94bbrsJiagKfdkjE8w3vQyMEicFeZSnP6K9JJ/132',
-            menu: ['首页', '设置', '登出']
+            userInfo: {
+                name: '路过一片云',
+                avatar: 'https://thirdwx.qlogo.cn/mmopen/sHGrggDoMxFfm6HEt7P6DRQ7AnxBJ7SgADdfhLib2V4z7iaGvNWytAZSMC9NCNZ94bbrsJiagKfdkjE8w3vQyMEicFeZSnP6K9JJ/132'
+            },
+            menuList: [
+                {
+                    title: '首页'
+                },
+                {
+                    title: '设置'
+                },
+                {
+                    title: '退出'
+                }
+            ]
         }
     },
     methods: {
