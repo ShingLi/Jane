eff1fd893db1532083bb439582646702a58b5081
shingli <745573545@qq.com>
2020-11-06 22:30:11 +0800

    web(layout/errpr.vue) 增加默认错误页面

---
+++ web/layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container"></div>
+</template>
+<script>
+export default {
+    name: 'Error',
+    props: ['error']
+}
+</script>
+<style lang="scss" scoped>
+    .container{
+        height: 100vh;
+        width: 100vw;
+    }
+</style>
