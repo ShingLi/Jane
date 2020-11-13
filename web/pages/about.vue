daa73912bcefdc9b35a28def412c87f37cfe321f
shingli <745573545@qq.com>
2020-11-13 22:42:15 +0800

    feat(web/about.vue) 关于我

--- web/pages/about.vue
+++ web/pages/about.vue
@@ -1,8 +1,30 @@
 <template>
-    <div></div>
+    <div class="about">
+        <div class="about__content">
+            s
+        </div>
+    </div>
 </template>
 <script>
 export default {
-    name: 'About'
+    name: 'About',
+    head: {
+        title: '关于',
+        meta: [
+            {
+                hid: 'about__description',
+                name: 'description',
+                content: '李成的个人描述'
+            },
+            {
+                hid: 'author',
+                name: 'author',
+                content: 'shingli'
+            }
+        ]
+    }
 }
 </script>
+<style lang="scss" scoped>
+    @import './assets/scss/about.scss';
+</style>
