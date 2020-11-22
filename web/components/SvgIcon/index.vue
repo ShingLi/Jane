93c5a0b81f435d6233b98ee3c049713eace78c9e
shingli <745573545@qq.com>
2020-11-22 13:53:24 +0800

    feat(web/svg-sprite) svgIcon

---
+++ web/components/SvgIcon/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <svg aria-hidden="true" class="icon">
+        <use :xlink:href="iconName"></use>
+    </svg>
+</template>
+<script>
+export default {
+    name: 'SvgIcon',
+    props: {
+        iconClass: {
+            type: String,
+            default: ''
+        }
+    },
+    computed: {
+        iconName () {
+            return `#icon-${this.iconClass}`
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .icon{
+        width: 1em;
+        height: 1em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+</style>
