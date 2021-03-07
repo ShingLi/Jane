0dd77da8103e624b89ec87e81b4d3fc4807689b3
shingli <745573545@qq.com>
2021-03-07 13:06:26 +0800

    chore(dev.js)

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,1 +1,13 @@
-// 开发环境
+import fs from 'fs-extra'
+import consola from 'consola'
+
+export default async function  run () {
+    const argv  = process.argv.slice(2)
+    if (argv.includes('web')) {
+        consola.info('执行', argv[0])
+        process.argv.splice(2)
+        
+    }
+}
+
+run()
