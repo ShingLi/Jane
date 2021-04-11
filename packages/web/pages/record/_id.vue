6319537167a206b9649f2c577a14afa8c86521ab
shingli <745573545@qq.com>
2021-04-12 00:27:14 +0800

    feat(web/record) 增加子路由

---
+++ packages/web/pages/record/_id.vue
@@ -0,0 +1,27 @@
+<template>
+    <transition name="side">
+        <div class="article">
+        
+        </div>
+    </transition>
+</template>
+<script>
+export default {
+    name: 'essay',
+    mounted () {
+        console.log('mounted')
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .article{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 3000px;
+        background-color: #ddd;
+        z-index: 5;
+        transition: all .4s;
+    }
+</style>
