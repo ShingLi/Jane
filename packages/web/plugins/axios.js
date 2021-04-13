c4370858b54846c37862155d1473cb7c65715a27
shingli <745573545@qq.com>
2021-04-13 23:01:40 +0800

    feat(web/plugins) axios

--- packages/web/plugins/axios.js
+++ packages/web/plugins/axios.js
@@ -1,4 +1,5 @@
-export default function ({ $axios, redirect }, inject) {
+import consola from 'consola'
+export default function ({ $axios, redirect, error }, inject) {
     /* licheng axios 全局处理
     -------------------------- */
     $axios.setHeader('Authorization', 'jane')
@@ -11,14 +12,14 @@ export default function ({ $axios, redirect }, inject) {
     })
 
     $axios.onResponse(response => {
-        const { data } = response
-        return data
+        const { data: { responseCode, responseMsg, responseData } } = response
+        if (responseCode == '0000') {
+            return responseData
+        }
     })
 
     $axios.onError(error => {
         // 这里错误
-        if (error == 9999) {
-            // redirect()
-        }
+        consola.error(error)
     })
 }
