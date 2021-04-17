d15caea8baacd0e81213c0a5473490a8ec5193d9
Shingli <745573545@qq.com>
2021-04-17 14:13:48 +0800

    feat(web/plugins) 增加Button 按钮的引用

--- packages/web/plugins/elementui.js
+++ packages/web/plugins/elementui.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
-import { Progress, Timeline, TimelineItem, Card } from 'element-ui'
+import { 
+    Progress,
+    Timeline,
+    TimelineItem,
+    Card,
+    Button
+} from 'element-ui'
 
 
-Vue.use(Progress).use(Timeline).use(TimelineItem).use(Card)
+Vue
+    .use(Progress)
+    .use(Timeline)
+    .use(TimelineItem)
+    .use(Card)
+    .use(Button)
