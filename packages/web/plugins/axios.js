87dc10746c697edcd26dc1efce2aa8f5c63d4c92
Shingli <745573545@qq.com>
2021-04-18 16:03:08 +0800

    fix(web/plugins) axios 增加打印

--- packages/web/plugins/axios.js
+++ packages/web/plugins/axios.js
@@ -1,25 +1,31 @@
 import consola from 'consola'
 export default function ({ $axios, redirect, error }, inject) {
-    /* licheng axios 全局处理
+    /* axios 全局处理
     -------------------------- */
     $axios.setHeader('Authorization', 'jane')
 
-    $axios.onRequest(config => {
-        if (process.env.NODE_ENV === 'development') {
-            // 从本地读取json
-        }
-        return config
-    })
+    // $axios.onRequest(config => {
+    //     if (process.env.NODE_ENV === 'development') {
+    //         // 从本地读取json
+    //     }
+    //     return config
+    // })
 
     $axios.onResponse(response => {
         const { data: { responseCode, responseMsg, responseData } } = response
         if (responseCode == '0000') {
             return responseData
+        } else {
+            if (process.client) {
+                consola.info('axios--服务端请求')
+            } else {
+                consola.info('axios--客户端请求')
+            }
         }
     })
 
     $axios.onError(error => {
         // 这里错误
-        consola.error(error)
+        consola.error('axios--onerror函数', error)
     })
 }
