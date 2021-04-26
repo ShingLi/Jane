5a48adca2947b345f608e9bde1ad0a1cbeca32ad
shingli <745573545@qq.com>
2021-04-26 22:52:19 +0800

    feat(admin/components) SvgIcon

---
+++ packages/admin/src/components/SvgIcon/SvgIcon.jsx
@@ -0,0 +1,28 @@
+const style = {
+    fill: 'currentColor',
+    overflow: 'hidden',
+    verticalAlign: '-0.15em',
+    width: '100%',
+    height: '100%'
+}
+export default {
+    name: 'SvgIcon',
+    props: {
+        iconName: {
+            type: String,
+            default: ''
+        }
+    },
+    computed: {
+        iconClass () {
+            return `#icon-${this.iconName}`
+        }
+    },
+    render () {
+        return (
+            <svg class="icon" aria-hidden="true" style={ { ...style } }>
+                <use xlink:href={ this.iconClass } ></use>
+            </svg>
+        )
+    }
+}
