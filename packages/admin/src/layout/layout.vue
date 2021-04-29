68b1bd661acf6f04c7b19e5435e7bbe33ffae735
shingli <745573545@qq.com>
2021-04-29 23:24:42 +0800

    docs(* readme)

--- packages/admin/src/layout/layout.vue
+++ packages/admin/src/layout/layout.vue
@@ -1,51 +1,199 @@
 <template>
     <div class="layout">
         <div class="layout__main">
-            <!-- <Sidebar /> -->
-            <div class="layout__content">
-                <router-view class="router"/>
+            <div class="sidebar">
+                <!-- <div class="radius"></div> -->
+            </div>
+            <div class="content">
+                <div class="navbar">
+                    <div class="tools">
+                        <i class="icon full--screen">
+                            <svg-icon iconName="full--screen" />
+                        </i>
+                    </div>
+                    <div class="userinfo">
+                        <Avatar
+                            class="avatar"
+                            :src="require('@/assets/images/avatar.jpeg')"
+                        />
+                        <el-dropdown>
+                            <div class="el-dropdown-link">
+                                <p>一片云</p>
+                                <i class="icon arrow--bottom">
+                                    <svg-icon
+                                        iconName="arrow--sj"
+                                        class="svg"
+                                    />
+                                </i>
+                            </div>
+                            <el-dropdown-menu slot="dropdown">
+                                <el-dropdown-item
+                                    icon="el-icon-s-promotion"
+                                    class="txt"
+                                >
+                                    <a href=""> 首页 </a>
+                                </el-dropdown-item>
+                                <el-dropdown-item
+                                    icon="el-icon-switch-button"
+                                    class="txt"
+                                    @click.native="signout"
+                                >
+                                    退出
+                                </el-dropdown-item>
+                            </el-dropdown-menu>
+                        </el-dropdown>
+                    </div>
+                </div>
+                <router-view class="router" />
             </div>
         </div>
     </div>
 </template>
 <script>
-// import Sidebar from 'components/sidebar/Sidebar'
+import Avatar from 'components/Avatar/Avatar'
+import SvgIcon from 'components/SvgIcon/SvgIcon.jsx'
 
 export default {
     name: 'Layout',
     components: {
-        // Sidebar
-    }
+        Avatar,
+        SvgIcon,
+    },
+    methods: {
+        signout() {
+            this.$confirm('是否要退出登录？', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning',
+            })
+                .then(() => {
+                    this.$store.dispatch('user/signout')
+                    this.$message({
+                        type: 'success',
+                        message: '已',
+                    })
+                })
+                .catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消退出',
+                    })
+                })
+        },
+    },
 }
 </script>
 <style lang="scss" scoped>
-    .layout{
+.layout {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    &__main {
+        width: 90vw;
+        max-width: 1250px;
+        min-width: 800px;
+        height: 85vh;
+        overflow: hidden;
+        border-radius: 12px;
         display: flex;
-        align-items: center;
-        justify-content: center;
+        background-color: #0e8bff;
+        box-shadow: 0 4px 6px #d2d2d2, 0 -2px 10px #d2d2d2;
         position: relative;
-        width: 100vw;
-        height: 100vh;
-        &__main{
-            width: 90vw;
-            max-width: 1250px;
-            height: 90vh;
-            overflow: hidden;
-            border-radius: 30px;
-            display: flex;
-            background-color: #0e8bff;
-            box-shadow: 0 4px 6px #d2d2d2, 0 -2px 10px #d2d2d2;
-            background-image: linear-gradient(45deg,#0ea6ff,#13adff);
-        }
-        &__content{
-            flex:1;
+        transition: all 0.3s ease-in-out;
+    }
+    .sidebar {
+        position: relative;
+        width: 230px;
+        height: 100%;
+        // background-color: #0e8bff;
+        .radius {
+            position: absolute;
+            width: 50px;
+            height: 50px;
+            bottom: 0px;
+            right: 0px;
             background-color: #fff;
-            border-radius: 30px;
-            overflow: hidden;
-            .router{
-                padding: 15px;
-                overflow-y: auto;
+            &::after {
+                content: '';
+                display: block;
+                width: 50px;
+                height: 50px;
+                border-radius: 0 0 17px 0;
+                background-color: #0e8bff;
             }
         }
     }
+    .content {
+        flex: 1;
+        background-color: #fff;
+        border-radius: 10px;
+        overflow: hidden;
+        border: 1px solid #ccc;
+        border-left: none;
+        .router {
+            padding: 15px;
+            overflow-y: auto;
+        }
+        .navbar {
+            position: relative;
+            width: 100%;
+            height: 60px;
+            display: flex;
+            justify-content: flex-end;
+            padding: 10px 20px;
+            align-items: center;
+            border-bottom: 1px dotted #48b4f1;
+            .userinfo {
+                display: flex;
+                align-items: center;
+                height: 100%;
+                padding-left: 15px;
+                border-left: 1px solid #ebebeb;
+                cursor: pointer;
+                .el-dropdown-link {
+                    display: flex;
+                    align-items: center;
+                    margin-left: 15px;
+                    > p {
+                        font-family: Jane;
+                        font-size: 15px;
+                        &:hover {
+                            color: #98bf21;
+                        }
+                    }
+                    .arrow--bottom {
+                        width: 10px;
+                        height: 10px;
+                        margin-left: 5px;
+                    }
+                }
+            }
+            .tools {
+                margin-right: 15px;
+                .full--screen {
+                    width: 25px;
+                    height: 25px;
+                    &:hover {
+                        color: #98bf21;
+                    }
+                }
+            }
+            .avatar {
+                width: 40px;
+                height: 40px;
+                border-radius: 50%;
+                border: 1px solid #bbb;
+                padding: 1px;
+                box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+            }
+        }
+    }
+}
+
+.txt {
+    text-transform: capitalize;
+}
 </style>
