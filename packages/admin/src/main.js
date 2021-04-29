91aa33a2ab875ca05c4f0a395424793c2c9bce1a
shingli <745573545@qq.com>
2021-04-29 23:28:00 +0800

    refactor(admin/main) 优化结构

--- packages/admin/src/main.js
+++ packages/admin/src/main.js
@@ -3,16 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import ElementUI from 'element-ui'
-
 import './permission.js'
-import http from 'config/http'
 
-import 'element-ui/lib/theme-chalk/index.css'
-import './assets/scss/index.scss'
+import { http } from 'plugins'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
 
 Object.defineProperty(Vue.prototype, '$http', {
     value: http
