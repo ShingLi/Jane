9aaa631f4bf4d7e085df48a7ede4c5d9c95e70aa
shingli <745573545@qq.com>
2021-04-29 23:30:38 +0800

    chore(admin/packages.json)

--- packages/admin/vue.config.js
+++ packages/admin/vue.config.js
@@ -1,8 +1,10 @@
 const path = require('path')
-const resolve = dir => path.resolve(__dirname, dir)
-console.log(__dirname)
+const fs = require('fs-extra')
 
-module.exports = {
+const resolve = dir => path.resolve(__dirname, dir)
+
+
+const config = {
     configureWebpack: config => {
         return {
             resolve: {
@@ -12,17 +14,40 @@ module.exports = {
                     router: resolve('src/router'),
                     store: resolve('src/store'),
                     utils: resolve('src/utils'),
-                    config: resolve('src/config')
+                    config: resolve('src/config'),
+                    plugins: resolve('src/plugins'),
+                    assets: resolve('src/assets')
                 }
             },
         }
     },
     chainWebpack: config => {
+        
+        config.module.rule('svg').exclude.add(resolve('src/assets/svg'))
+
         config
             .plugin('html')
-            .tap(args => {
-                args[0].title = '一片云'
-                return args
-            })
-    }
+                .tap(args => {
+                    args[0].title = '一片云'
+                    return args
+                })
+                .end()
+            .module
+                .rule('svg-loader')
+                .test(/\.svg$/)
+                .include
+                .add(resolve('src/assets/svg'))
+                .end()
+                .use('svg-sprite-loader')
+                .loader('svg-sprite-loader')
+                .options({
+                    symbolId: 'icon-[name]'
+                })
+                .end()
+
+                console.log(config.toString())
+    },
+    productionSourceMap: false, // 去掉生产sourcemap
 }
+
+module.exports = config
