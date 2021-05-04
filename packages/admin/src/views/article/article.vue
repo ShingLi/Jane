8e5e8b376f3d97ff7c3b764b86632f58407384dc
Shingli <745573545@qq.com>
2021-05-04 20:25:11 +0800

    feat(admin/文章) 增加文章编辑页面

---
+++ packages/admin/src/views/article/article.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="article">
+        <Tag />
+        <div class="articlewrap">
+            
+        </div>
+    </div>
+</template>
+<script>
+import Tag from 'components/Tag/Tag'
+export default {
+    name: 'Article',
+    components: {
+        Tag
+    }
+}
+</script>
+<style lang="scss" scoped>
+    @import './scss/article.scss';
+</style>
