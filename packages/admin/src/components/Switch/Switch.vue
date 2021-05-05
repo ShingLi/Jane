2ff00f8b00b8bba0bcfe877391173496a521beee
Shingli <745573545@qq.com>
2021-05-05 17:44:13 +0800

    feat(admin/components) switch

--- packages/admin/src/components/Switch/Switch.vue
+++ packages/admin/src/components/Switch/Switch.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="lc__switch">
-        <label for="lc__switch"></label>
-        <input type="checkbox" id="lc__switch">
-    </div>
+    <label for="lc__switch" class="switch">
+        <input type="checkbox" id="lc__switch" class="switch--input"/>
+        <span class="switch--core"></span>
+    </label>
 </template>
 <script>
 export default {
@@ -10,9 +10,42 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-    .lc__switch{
-        height: 35px;
+    .switch{
+        display: inline-block;
+        cursor: pointer;
+    }
+    .switch--core{
+        display: inline-block;
+        height: 32px;
         width: 52px;
-        border: 1px solid #fff;
+        border-radius: 25px;
+        position: relative;
+        border: 1px solid #d9d9d9;
+        background: #d9d9d9;
+        transition: all .3s ease-in-out;
+        &::before{
+            position: absolute;
+            content: '';
+            top: 0;
+            left: 0;
+            width: 30px;
+            height: 30px;
+            border-radius: 50%;
+            background-color: #fff;
+            transition: all .3s ease-in-out;
+        }
+    }
+    .switch--input{
+        width: 0;
+        height: 0;
+        position: relative;
+        display: none;
+        &:checked + .switch--core {
+            background-color: #13ce66;
+            border-color: #13ce66;
+            &::before{
+                transform: translateX(20px);
+            }
+        }
     }
 </style>
