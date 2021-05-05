31a80238d06253ac2cbb67cefb4f770e89f30a66
Shingli <745573545@qq.com>
2021-05-05 10:56:22 +0800

    feat(admin/Tag) Tag.vue

---
+++ packages/admin/src/components/Tag/Tag.vue
@@ -0,0 +1,23 @@
+<template>
+    <h2>{{ text }}</h2>
+</template>
+<script>
+export default {
+    name: 'Tag',
+    props: {
+        text: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    h2{
+        font-size: 17px;
+        line-height: 23px;
+        color: #0e8bff;
+        padding-left: 18px;
+        border-left: 2px solid #0084ff;
+    }
+</style>
