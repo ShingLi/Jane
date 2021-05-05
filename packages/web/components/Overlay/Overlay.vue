e9de8b2c14901a201f0e1bf8be3d504ab3b4b28f
Shingli <745573545@qq.com>
2021-05-05 10:54:31 +0800

    feat(web/components) overlay

---
+++ packages/web/components/Overlay/Overlay.vue
@@ -0,0 +1,28 @@
+<template>
+    <transition name="fadeIn">
+        <div class="overlay">
+            <slot />
+        </div>
+    </transition>
+</template>
+<script>
+    export default {
+        name: 'Overlay',
+    }
+</script>
+<style lang="scss" scoped>
+    .overlay{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 10;
+        background-color: rgba(0, 0, 0, .5);
+        transition: all .5s ease-in-out;
+    }
+
+    .fadeIn{
+        background-color: rgba(0, 0, 0, 0);
+    }
+</style>
