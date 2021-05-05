bc379f37b68e846464b40985850184b0d02cfa84
Shingli <745573545@qq.com>
2021-05-05 10:54:04 +0800

    style(web/pages) index.vue 文案措辞修改

--- packages/web/pages/index.vue
+++ packages/web/pages/index.vue
@@ -3,7 +3,7 @@
         <div class="tips">
             <p><a href="//beian.miit.gov.cn" target="_blank">皖ICP备18000360号</a></p>
             <transition name="fade">
-                <h4 v-if="!isCollapse" class="copy fast">遗憾吗？那个你曾忙完就想找她聊天的人，终于和你无话可说了。那一切好像开始了，又好像从没开始！始于陌生，归于陌生是你和她所有的故事</h4>
+                <h4 v-if="!isCollapse" class="copy fast">那个菇凉，她好像从没主动找你聊过日常。一切好像曾开始过，但！陌生到陌生或许是你和她所有的故事</h4>
             </transition>
         </div>
         <canvas class="canvas"></canvas>
