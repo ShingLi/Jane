6642007efbdcb51cc774ed6999816273968aa3e9
Shingli <745573545@qq.com>
2021-05-05 10:56:48 +0800

    feat(adminSwitch) Switch.vue

---
+++ packages/admin/src/components/Switch/Switch.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="lc__switch">
+        <label for="lc__switch"></label>
+        <input type="checkbox" id="lc__switch">
+    </div>
+</template>
+<script>
+export default {
+    name: 'Switch'
+}
+</script>
+<style lang="scss" scoped>
+    .lc__switch{
+        height: 35px;
+        width: 52px;
+        border: 1px solid #fff;
+    }
+</style>
