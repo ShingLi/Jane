7795ced32f12674b77355ffc7f96a33a90e022fa
Shingli <745573545@qq.com>
2021-05-05 10:55:00 +0800

    style(admin/article) 文章

--- packages/admin/src/views/article/article.vue
+++ packages/admin/src/views/article/article.vue
@@ -1,8 +1,20 @@
 <template>
     <div class="article">
-        <Tag />
+        <Tag :text="text"/>
         <div class="articlewrap">
-            
+            <el-input placeholder="请输入文章标题"
+                clearable
+                prefix-icon="el-icon-paperclip"
+                class="title"
+                type=”text“
+                v-model="formData.title"
+            />
+            <div class="markdown">
+                <mavon-editor
+                    v-model="formData.value"
+                    :subfield="false"
+                />
+            </div>
         </div>
     </div>
 </template>
@@ -12,6 +24,15 @@ export default {
     name: 'Article',
     components: {
         Tag
+    },
+    data () {
+        return {
+            text: '夜泊秦淮近酒家，商女不知亡国恨',
+            formData: {
+                title: '',
+                value: ''
+            }
+        }
     }
 }
 </script>
