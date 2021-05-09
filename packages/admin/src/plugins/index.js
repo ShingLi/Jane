776177a06fb909f8cbdf2b378bd25311f3fee2b0
shingli <745573545@qq.com>
2021-05-09 23:02:25 +0800

    refactor(admin/plugins) 优化

--- packages/admin/src/plugins/index.js
+++ packages/admin/src/plugins/index.js
@@ -1,17 +1,16 @@
 import Vue from 'vue'
 import mavonEditor from 'mavon-editor'
-import SvgIcon from 'components/SvgIcon/SvgIcon'
-import LcSwitch from 'components/Switch/Switch'
+
 import CollapseTransition from 'element-ui/lib/transitions/collapse-transition'
 
 import 'mavon-editor/dist/css/index.css'
 
 import './svg'
 import './element-ui'
+import './auto-components'
 
 Vue.use(mavonEditor)
-Vue.component('SvgIcon', SvgIcon)
-Vue.component('LcSwitch', LcSwitch)
+
 Vue.component(CollapseTransition.name, CollapseTransition)
 
 export { default as http } from './axios'
