e60a885cba7d989c431a4db64c0650c9c6ea9495
shingli <745573545@qq.com>
2021-06-02 23:32:27 +0800

    chore(serve/models) 文章文档

---
+++ packages/server/models/article.js
@@ -0,0 +1,54 @@
+module.exports = (Schema, model) => {
+    const aritcleSchema = new Schema ({
+        // 文章id
+        id: {
+            type: Number,
+            default: 0,
+        },
+        // 文章标题
+        title: {
+            type: String,
+            required: true,
+            default: ''
+        },
+        // 文章seo关键字
+        seo: {
+            type: String,
+            default: ''
+        },
+        // 文章内容
+        content: {
+            type: String,
+            required: true,
+            default: ''
+        },
+        // 文章背景音乐地址
+        musicUrl: {
+            type: String,
+            default: ''
+        },
+        // 文章封面图片
+        picUrl: {
+            type: String,
+            default: ''
+        },
+        // 文章创建时间
+        createTime: {
+            type: String,
+            default: '',
+            required: true,
+        },
+        // 文章更新时间
+        updatedTime: {
+            type: String,
+            default: '',
+            required: true
+        },
+        // 阅读数
+        readNum: Number,
+        // 喜欢数
+        likeNum: Number,
+    })
+
+    return model('Article', aritcleSchema)
+}
