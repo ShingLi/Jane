07e72046a9692083773097706bbb5168b7ac9159
shingli <745573545@qq.com>
2021-06-05 21:24:32 +0800

    feat(admin/views) comment

---
+++ packages/admin/src/views/comment/comment.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="comment">
+        <Tag :text="text" />
+        <div class="main">
+            
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Comment',
+    data () {
+        return {
+            text: '若是气质藏于身，岁月从不败美人'
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    @import './scss/comment.scss';
+</style>
