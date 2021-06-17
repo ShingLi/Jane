5ef6c1814c306cb8cfa1ab3df4d5a967465c3afd
shingli <745573545@qq.com>
2021-06-17 23:19:11 +0800

    */other

--- packages/server/utils/index.js
+++ packages/server/utils/index.js
@@ -1,9 +1,30 @@
 import f from './f'
 import consola from 'consola'
 
-const fn ={
+
+const parseQuery = (req) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const requestType = req.method
+            let data = {}
+            if (requestType === 'post') {
+                data = req.body
+            } else {
+                data = req.query
+            }
+            if (!Object.keys(data).length) {
+                
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+const fn = {
     f,
-    consola
+    consola,
+    parseQuery
 }
 
 const utils = {
