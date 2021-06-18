72ffb5ff997686228f92d79f264680515c98d0ca
shingli <745573545@qq.com>
2021-06-18 22:31:00 +0800

    fix(web/plugins) ? 高亮

--- packages/web/plugins/markdown.js
+++ packages/web/plugins/markdown.js
@@ -1,10 +1,27 @@
 import MarkdownIt from 'markdown-it'
-import Prism from 'prismjs'
+import prism from 'prismjs'
 
+// import Prism  from 'markdown-it-prism'
+// import Prism from 'prismjs'
+// import 'prismjs/themes/prism-dark.css'
 import 'prismjs/themes/prism-okaidia.css'
-
+// import hljs from 'highlight.js'
+// import 'highlight.js/styles/dark.css'
+// import 'highlight.js/styles/monokai.css'
+function wrap(code, lang) {
+    // if (lang === 'text') {
+    //     code = escapeHtml(code)
+    // }
+    return `<pre v-pre class="language-${lang}"><code>${code}</code></pre>`
+}
 const md = new MarkdownIt({
-    breaks: true
+    breaks: true,
+    highlight(str, lang) {
+        if (prism.languages[lang]) {
+            const code = prism.highlight(str, prism.languages[lang], lang)
+            return wrap(code, lang)
+        }
+    }
 })
 
 // 代码块中增加类名
