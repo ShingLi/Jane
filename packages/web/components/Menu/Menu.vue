27d651bd3a8371def4c76924a39402eb93c6da35
shingli <745573545@qq.com>
2021-06-21 23:44:24 +0800

    feat(web/components) menu

--- packages/web/components/Menu/Menu.vue
+++ packages/web/components/Menu/Menu.vue
@@ -1,8 +1,24 @@
 <template>
     <div class="menu">
-        <div class="Svg__content">
-            <svg-icon :icon-class="className" @click.native.stop="isCollapse = !isCollapse"/>
+        <div class="fixed--top">
+            <transition name="fade">
+                <div class="left sfast" v-if="wide">
+                    <div class="back" @click="$router.push({ path: '/record.html' })">
+                        <svg-icon iconClass="back2" />
+                    </div>
+                </div>
+            </transition>
+            <div class="process"></div>
+            <div class="right">
+                <svg-icon :icon-class="className" @click.native.stop="isCollapse = !isCollapse"/>
+            </div>
+            <transition name="fade">
+                <p class="line sfast" v-if="wide">
+                    <span class="liner" :style="calcWidth"></span>
+                </p>
+            </transition>
         </div>
+        
         <transition name="fadeIndown">
             <div v-show="isCollapse" class="mask">
                 <div class="mask__container">
@@ -28,6 +44,16 @@
 import { mapMutations } from 'vuex'
 export default {
     name: 'Menu',
+    props: {
+        scrollTop: {
+            type: Number,
+            default: 0
+        },
+        scrollHeight: {
+            type: Number,
+            default: 0
+        }
+    },
     data () {
         return {
             isCollapse: false,
@@ -50,6 +76,7 @@ export default {
                     href: 'https://github.com/shingli'
                 }
             ],
+            wide: false
         }
     },
     computed: {
@@ -63,37 +90,106 @@ export default {
 
             return icon
         },
+        calcWidth () {
+            let width = 0, canscrollTop = this.scrollHeight - document.documentElement.clientHeight
+            if (this.scrollTop) {
+                width = (this.scrollTop / canscrollTop) * 100
+            }
+            return {
+                width: `${width}%`,
+            }
+        }
+    },
+    watch: {
+        $route (val) {
+            this.validate()
+        }
+    },
+    mounted () {
+        this.validate()
     },
     methods: {
         navigator () {
             this.isCollapse = false
             this.menuClick(true)
         },
+        validate () {
+            if (this.$route.name == 'essays-id') {
+                setTimeout(() => {
+                    this.wide = true
+                },2000)
+            } else {
+                this.wide = false
+            }
+        },
         ...mapMutations({
             menuClick: 'changeClick'
-        })
+        }),
     }
 }
 </script>
 <style lang="scss" scoped>
     .menu{
         position: fixed;
-        top: 15px;
-        right: 15px;
-        width: 30px;
-        height: 30px;
-        z-index: 9;
-        cursor: pointer;
-        .Svg{
-            &__content{
-                width: 100%;
-                height: 100%;
+        height: 55px;
+        left: 0;
+        width: 100%;
+        z-index: 3;
+        border-bottom: 1px solid transparent;
+        &.border-1px{
+            border-bottom-color: #f0f0f0;
+        }
+        .fixed--top{
+            top: 0;
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            display: flex;
+            align-items: center;
+            padding: 0 22px;
+            // backdrop-filter: blur(0px);
+            .line{
+                height: 1px;
                 position: absolute;
-                z-index: 3;
-                ::v-deep .icon{
+                bottom: 0;
+                width: 100%;
+                background-color: #f0f0f0;
+                .liner{
+                    position: relative;
+                    display: block;
+                    width: 0%;
+                    height: 2px;
+                    border-radius: 4px;
+                    top: -0.5px;
+                    background-color: #1a73e8;
+                    transition: all .4s linear;
+                }
+            }
+            div{
+                height: 100%;
+                display: flex;
+                align-items: center;
+                &.process{
+                    flex: 1;
+                }
+                &.right,&.left{
+                    cursor: pointer;
+                }
+                &.right{
                     width: 30px;
-                    height:30px;
-                    // animation: suofang 2s ease-in-out infinite;
+                    position: relative;
+                    ::v-deep{
+                        .svg{
+                            position: absolute;
+                            top: 50%;
+                            transform: translateY(-50%);
+                            z-index: 4;
+                        }
+                    }
+                }
+                ::v-deep .svg{
+                    width: 30px;
+                    height: 30px;
                 }
             }
         }
@@ -103,6 +199,7 @@ export default {
             height: 100%;
             left: 0;
             width: 100%;
+            z-index: 3;
             &__container{
                 position: relative;
                 width: 100%;
