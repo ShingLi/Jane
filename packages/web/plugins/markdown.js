353a672ccc63ed9ebd159862f3683c9ac83f3fa3
Shingli <745573545@qq.com>
2021-07-03 22:08:48 +0800

    feat(web/plugins) markdown.js

--- packages/web/plugins/markdown.js
+++ packages/web/plugins/markdown.js
@@ -1,30 +1,37 @@
 import MarkdownIt from 'markdown-it'
 import prism from 'prismjs'
 
-// import Prism  from 'markdown-it-prism'
-// import Prism from 'prismjs'
-// import 'prismjs/themes/prism-dark.css'
 import 'prismjs/themes/prism-okaidia.css'
-// import hljs from 'highlight.js'
-// import 'highlight.js/styles/dark.css'
-// import 'highlight.js/styles/monokai.css'
+
+import loadLanguages from 'prismjs/components/index'
+
+loadLanguages(['markup', 'css', 'javascript','jq', 'php', 'scss', 'jsx'])
+
 function wrap(code, lang) {
-    // if (lang === 'text') {
-    //     code = escapeHtml(code)
-    // }
+    if (lang === 'text') {
+        // code = escapeHtml(code)
+    }
     return `<pre v-pre class="language-${lang}"><code>${code}</code></pre>`
 }
 const md = new MarkdownIt({
     breaks: true,
     highlight(str, lang) {
+        if (!lang) {
+            return wrap(str, 'text')
+        }
+        if (!prism.languages[lang]) {
+            try {
+              loadLanguages([lang])
+            } catch (e) {
+                console.log('不支持高亮', e)
+            }
+          }
         if (prism.languages[lang]) {
             const code = prism.highlight(str, prism.languages[lang], lang)
             return wrap(code, lang)
         }
+        return wrap(str, 'text')
     }
 })
-
-// 代码块中增加类名
-// md.use(Prism)
 
 export default md
