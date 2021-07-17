b9b6377af5fb6576cc4fc2728643b3ce0fe6a131
jane <745573545@qq.com>
2021-07-17 18:17:38 +0800

    feat(serve/models) 上传图片文案修改

--- packages/server/models/Upload.js
+++ packages/server/models/Upload.js
@@ -1,12 +1,12 @@
 module.exports = (Schema, model) => {
     
     const uploadSchema = new Schema({
-        fieldname: String,
-        originalname: String,
-        mimetype: String,
-        filename: String,
-        path: String,
-        date: {
+        upload_field_name: String,
+        upload_original_name: String,
+        upload_mimetype: String,
+        upload_filename: String,
+        upload_path: String,
+        upload_date: {
             type: String,
             default: new Date().toLocaleString()
         }
